1670c637ef9ffc43854e22d5a2a1e453902d2d3c
irene <irene.xu2@etu.u-paris.fr>
2025-03-14 22:04:27 +0100

    Update Connexion avec les données de la bd + update string.xml

--- app/src/main/java/com/example/appfranceassossante/fragments/SeConnecterFragment.kt
+++ app/src/main/java/com/example/appfranceassossante/fragments/SeConnecterFragment.kt
@@ -22,37 +22,54 @@ class SeConnecterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        mongoDBConnection = MongoDBConnection()
+
         val view = inflater.inflate(R.layout.fragment_se_connecter, container, false)
 
         val btnconnecter = view.findViewById<Button>(R.id.se_connecter)
         val mail = view.findViewById<EditText>(R.id.co_mail)
         val mdp = view.findViewById<EditText>(R.id.co_mdp)
 
-        btnconnecter.setOnClickListener{
+        btnconnecter.setOnClickListener {
             val mail = mail.text.toString()
             val motDePasse = mdp.text.toString()
 
-            var user = mongoDBConnection.findUserByEmail(mail)
+            if (mail.isEmpty() || motDePasse.isEmpty()) {
+                Toast.makeText(
+                    context,
+                    getString(R.string.error_message_champs_vides),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                var user = mongoDBConnection.findUserByEmail(mail)
 
-            if(user != null){
-                if(motDePasse.equals(user.mdp)){
-                    userViewModel.setCivilite(user.civilite)
-                    userViewModel.setNom(user.nom)
-                    userViewModel.setPrenom(user.prenom)
-                    userViewModel.setMail(user.email)
-                    userViewModel.setMdp(user.mdp)
-                    userViewModel.setHandicap(user.handicap)
-                    val transaction = requireActivity().supportFragmentManager.beginTransaction()
-                    // remplace le fragment actuel par le fragment qui suit ("ProfilFragment")
-                    transaction.replace(R.id.fragment_container, ProfilFragment())
-                    transaction.addToBackStack(null) // ajoute le fragment actuel au backstack (pour pouvoir retourner dessus quand on fait retour sur le tel)
-                    transaction.commit()
-                }
-                else
-                    Toast.makeText(context,  getString(R.string.error_message_mdp_incorrect), Toast.LENGTH_SHORT).show()
+                if (user != null) {
+                    if (motDePasse.equals(user.mdp)) {
+                        userViewModel.setCivilite(user.civilite)
+                        userViewModel.setNom(user.nom)
+                        userViewModel.setPrenom(user.prenom)
+                        userViewModel.setMail(user.email)
+                        userViewModel.setMdp(user.mdp)
+                        userViewModel.setHandicap(user.handicap)
+                        val transaction =
+                            requireActivity().supportFragmentManager.beginTransaction()
+                        // remplace le fragment actuel par le fragment qui suit ("ProfilFragment")
+                        transaction.replace(R.id.fragment_container, ProfilFragment())
+                        transaction.addToBackStack(null) // ajoute le fragment actuel au backstack (pour pouvoir retourner dessus quand on fait retour sur le tel)
+                        transaction.commit()
+                    } else
+                        Toast.makeText(
+                            context,
+                            getString(R.string.error_message_mdp_incorrect),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                } else
+                    Toast.makeText(
+                        context,
+                        getString(R.string.error_message_user_non_existant),
+                        Toast.LENGTH_SHORT
+                    ).show()
             }
-            else
-                Toast.makeText(context,  getString(R.string.error_message_user_non_existant), Toast.LENGTH_SHORT).show()
         }
 
         val btnsinscrire = view.findViewById<Button>(R.id.sinscrire)
