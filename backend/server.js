9457b713a9f2bfc5f6740e1b41633b45c3e8f18c
lucie <purplelucie94@gmail.com>
2025-03-23 15:55:53 +0100

    server.js avec les routes

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/userRoutes');
+const assosRoutes = require('./routes/assosRoutes');
+const donRoutes = require('./routes/donRoutes');
+
+require('dotenv').config();
+const app = express(); // crée le serveur express qui va config les routes
+app.use(express.json()); // transforme les requetes http au format json
+
+// Connexion à MongoDB
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connecté à MongoDB');
+  })
+  .catch((err) => {
+    console.error('Erreur de connexion à MongoDB:', err);
+  });
+
+
+// Utiliser les routes pour les utilisateurs
+// Utilisation des routes
+app.use('/users', userRoutes);
+app.use('/associations', assosRoutes);
+app.use('/donations', donRoutes);
+
+// Démarrer le serveur
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log('Serveur en écoute sur le port ${PORT}');
+});
