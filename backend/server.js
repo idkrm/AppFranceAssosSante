755b74344056dc76f553d08321b9552f3e2c0dfd
Mei-ling <mei-ling.wu@etu.u-paris.fr>
2025-03-31 15:43:32 +0200

    code qr

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Association = require('./models/Assos');
+const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
 const assosRoutes = require('./routes/assosRoutes');
@@ -9,6 +10,7 @@ const donRoutes = require('./routes/donRoutes');
 require('dotenv').config();
 const app = express(); // crée le serveur express qui va config les routes
 app.use(express.json()); // transforme les requetes http au format json
+app.use(cors()); //debloque les requêtes venant d’autre site (elles sont bloquées par defaut)
 
 // Connexion à MongoDB
 const uri = process.env.MONGO_URI;
@@ -26,9 +28,31 @@ app.use('/users', userRoutes);
 app.use('/associations', assosRoutes);
 app.use('/donations', donRoutes);
 
+//code qr
+const deepLinkUrl = "monapp://deeplink/page";  //deep link de l'app
+const playStoreUrl = "https://play.google.com/store/apps";  //url du play store
+
+app.get("/redirect", (req, res) => {
+  console.log("Redirection demandée !");
+  res.send(`
+    <html>
+      <head>
+        <title>Redirection...</title>
+        <script>
+          window.location.href = "${deepLinkUrl}";
+          setTimeout(() => { window.location.href = "${playStoreUrl}"; }, 2000);
+        </script>
+      </head>
+      <body>
+        <p>Si la redirection ne fonctionne pas, <a href="${playStoreUrl}">cliquez ici</a>.</p>
+      </body>
+    </html>
+  `);
+});
+
+
 // Démarrer le serveur
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur le port ${PORT}`);
 });
-
